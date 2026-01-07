56e1966887df4753664af77000a035f827fd76db
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2026-01-08 00:17:30 +0100

    chore(libs/core/webpack): add test configs

---
+++ packages/core/webpack/webpack.acceptance.babel.js
@@ -0,0 +1,12 @@
+import { getWebpackProductionSetup } from '@papillonbits/setup/webpack'
+import { environmentVariablesFilePaths, micrositeFolderPaths, micrositeUrlPaths } from './webpack.constant.babel'
+import { webpackCommonSetup } from './webpack.common.babel'
+import packageJSON from '../../../package.json'
+
+export default getWebpackProductionSetup({
+  webpackCommonSetup,
+  environmentVariablesFilePath: environmentVariablesFilePaths.acceptance,
+  micrositeUrlPath: micrositeUrlPaths.acceptance,
+  micrositeFolderPaths,
+  packageJSON,
+})
