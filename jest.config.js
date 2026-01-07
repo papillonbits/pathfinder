9e594f61385a7527849155abe5fe1f16899f4b22
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2026-01-08 00:17:25 +0100

    chore(config/jest): add jest configuration

---
+++ jest.config.js
@@ -0,0 +1,33 @@
+// https://docs.codecov.com/docs/codecov-uploader
+/* eslint-disable prefer-destructuring */
+const getJestSetup = require('@papillonbits/setup/jest').getJestSetup
+
+const testPathIgnorePatterns = process.env.NODE_ENV === 'test' ? ['packages/core/webpack.test.babel.js'] : ['packages/core']
+const coverageDirectory = './.coverage/'
+const collectCoverage = process.env.NODE_ENV === 'test'
+const collectCoverageFrom = [
+  '!**/.coverage/**',
+  '!**/.husky/**',
+  '!**/.mock/**',
+  '!**/.storybook/**',
+  '!**/.storybook-static/**',
+  '!**/node_modules/**',
+  'packages/**/*.{js,jsx}',
+  '!packages/**/index.js',
+  '!packages/**/*.prop.js',
+  '!packages/**/*.story.js',
+  '!packages/**/*.test.js',
+  '!packages/**/build/**',
+  '!packages/**/node_modules/**',
+  '!packages/**/webpack*',
+]
+const coverageThreshold = {
+  global: {
+    statements: 92,
+    branches: 63,
+    functions: 95,
+    lines: 91,
+  },
+}
+
+module.exports = getJestSetup({ testPathIgnorePatterns, coverageDirectory, collectCoverage, collectCoverageFrom, coverageThreshold })
