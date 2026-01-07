80541028ab44d9b5242fa018edbdaef2a0cc1af3
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2026-01-08 00:17:26 +0100

    feat(libs/core/library/constant/alert): add list gnomes alerts

---
+++ packages/core/src/library/constant/alertText/listGnomes/index.js
@@ -0,0 +1,15 @@
+export const alertTextListGnomes = Object.freeze({
+  pagination: { browsing: 'Browsing through list gnomes...' },
+  display: {
+    noGnomes: 'No list gnomes!',
+  },
+  action: {
+    search: {
+      progress: 'Searching gnomes...',
+    },
+    set: {
+      progress: 'Setting list gnomes...',
+      success: 'Successfully set list gnomes!',
+    },
+  },
+})
