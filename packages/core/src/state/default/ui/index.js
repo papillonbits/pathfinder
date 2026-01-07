089512dcc5267b7cb3b2afb930724d2fa1a30f79
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2026-01-08 00:17:27 +0100

    feat(libs/core/state/default): add ui defaults

---
+++ packages/core/src/state/default/ui/index.js
@@ -0,0 +1,22 @@
+export const ui = {
+  listGnomes: {
+    pagination: {
+      currentPage: {
+        canMoveBackwards: false,
+        canMoveForward: true,
+        currentIndex: 0,
+        indexItems: [],
+      },
+      pageNumber: null,
+      pageSize: null,
+    },
+    gnomesObjects: [],
+    search: {
+      hairColor: null,
+      profession: null,
+      gnomesObjects: null,
+    },
+    sort: null,
+    edit: {},
+  },
+}
